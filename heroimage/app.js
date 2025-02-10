25590bc318708b54674d4271f8126adc88da30b5
sergiortiz005 <sergiortiz059@gmail.com>
2025-02-10 18:23:50 +0100

    hero

--- heroimage/app.js
+++ heroimage/app.js
@@ -3,14 +3,16 @@ const slides = document.querySelectorAll('.slide');
 const buttons = document.querySelectorAll('.boton');
 
 function changeSlide(index) {
-    // Eliminar la clase active del slide anterior y del botón
-    slides[currentSlide].classList.remove('active');
+    slides[currentSlide].classList.add('outgoing');
     buttons[currentSlide].classList.remove('active');
 
-    // Agregar la clase active al nuevo slide y al botón correspondiente
-    slides[index].classList.add('active');
-    buttons[index].classList.add('active');
-
-    // Actualizar el índice de la diapositiva actual
-    currentSlide = index;
+    setTimeout(() => {
+        slides[currentSlide].classList.remove('outgoing');
+        slides[currentSlide].classList.remove('active');
+        
+        slides[index].classList.add('active');
+        buttons[index].classList.add('active');
+        
+        currentSlide = index;
+    }, 500); 
 }
